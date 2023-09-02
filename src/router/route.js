ec7ddc306b51fa07ad542f5586c831691fa92547
veronica17-web <veronicamiryala001@gmail.com>
2023-09-02 16:18:45 +0530

    first commit

---
+++ src/router/route.js
@@ -0,0 +1,8 @@
+const express = require("express")
+let router = express.Router()
+
+router.get("/test-me", function (req, res) {
+    res.send("this is successfully created");
+  });
+
+module.exports = router
