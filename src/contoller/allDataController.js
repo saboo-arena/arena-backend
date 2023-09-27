798a57c9ab341525c5320297916707734eabfbfd
veronica17-web <veronicamiryala001@gmail.com>
2023-09-27 15:40:49 +0530

    sorted allData descending order

--- src/contoller/allDataController.js
+++ src/contoller/allDataController.js
@@ -7,27 +7,28 @@ const popupModel = require("../model/popupModel");
 const serviceModel = require("../model/serviceModel");
 
 const allData = async (req, res) => {
-  try {
-    // Use Promise.all to query data from multiple collections concurrently
-    const data = await Promise.all([
-      corporateModel.find().exec(),
-      drivingSchoolModel.find().exec(),
-      financeModel.find().exec(),
-      insuranceModel.find().exec(),
-      onRoadPriceModel.find().exec(),
-      popupModel.find().exec(),
-      serviceModel.find().exec(),
-    ]);
-
-    // Combine the results into a single array
-    const combinedData = data.reduce((acc, curr) => acc.concat(curr), []);
-
-    // Return the combined data in the response
-    return res.status(200).send({ status: true, data: combinedData });
-  } catch (error) {
-    return res.status(500).send({ status: false, message: error.message });
-  }
-};
+    try {
+      // Use Promise.all to query data from multiple collections concurrently
+      const data = await Promise.all([
+        corporateModel.find().sort({ createdAt: -1 }).exec(),
+        drivingSchoolModel.find().sort({ createdAt: -1 }).exec(),
+        financeModel.find().sort({ createdAt: -1 }).exec(),
+        insuranceModel.find().sort({ createdAt: -1 }).exec(),
+        onRoadPriceModel.find().sort({ createdAt: -1 }).exec(),
+        popupModel.find().sort({ createdAt: -1 }).exec(),
+        serviceModel.find().sort({ createdAt: -1 }).exec(),
+      ]);
+  
+      // Combine the results into a single array
+      const combinedData = data.reduce((acc, curr) => acc.concat(curr), []);
+  
+      // Return the combined data in the response
+      return res.status(200).send({ status: true, data: combinedData });
+    } catch (error) {
+      return res.status(500).send({ status: false, message: error.message });
+    }
+  };
+  
 //===========================================================================
 
 const findDuplicates = async (
