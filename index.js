582b46bb7fbf8b2e9a790260fb2f53e4094666b7
veronica17-web <veronicamiryala001@gmail.com>
2023-10-09 16:17:15 +0530

    resloved the date tim error

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ const app = express();
 
 // Use rate limiting middleware to limit the rate of incoming requests
 const limiter = rateLimit({
-  windowMs: 2000, // 10 seconds
-  max: 5, // Limit to 5 requests per windowMs
+  windowMs: 2000, // 2 seconds
+  max: 5, // Limit to 5 requests per 2 seconds
   message: "Too many requests from this IP, please try again later.",
 });
 
 app.use(limiter);
 
-app.use(express.json());
-var cors = require("cors");
+app.use(express.json()); // This middleware will parse JSON data in the request body
+
+const cors = require("cors");
 app.use(cors());
 
 mongoose
@@ -30,6 +31,20 @@ mongoose
   )
   .then(() => console.log("MongoDb is connected"))
   .catch((err) => console.log(err));
+
+// Middleware to handle headers for all routes
+app.use((req, res, next) => {
+  const acceptHeader = req.get('Accept'); // Access the 'Accept' header
+  const refererHeader = req.get('Referer'); // Access the 'Referer' header
+
+  // Handle headers as needed
+  console.log('Accept Header:', acceptHeader);
+  console.log('Referer Header:', refererHeader);
+
+  // Continue processing the request
+  next();
+});
+
 app.use("/", route);
 
 app.listen(process.env.PORT || 3001, function () {
